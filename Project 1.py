19c14538503f92537cf4861e912d4c47e1c412e6
Insaf933 <insafturkmani933@gmail.com>
2024-10-30 19:50:32 +0200

    the main classes for detailes od drivers and cities with thei neighbors

---
+++ Project 1.py
@@ -0,0 +1,11 @@
+class Drivers:
+    def __init__(self,workId,name,startCity):
+        self.workId=workId
+        self.driver_name=driver_name
+        self.startCity=startCity
+
+class City:
+    def __init__(self, name, neighbors):
+        self.name = name
+        self.neighbors = neighbors
+
